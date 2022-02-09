b728e72bf2220887fa4da9b33e65f3d368523428
annalindqvist <annalindqvist_@live.se>
2022-02-09 09:48:10 +0100

    app2.js update for backup

--- app2.js
+++ app2.js
@@ -34,6 +34,7 @@ let activeDistanceBtn = document.getElementsByName("activeDistanceBtn");
 // data and searchResult variables
 let restaurantData;
 let searchResult = [];
+let inputSearch = [];
 
 
 // -------------------------------------------------------------------------
@@ -74,9 +75,157 @@ submitBtn.addEventListener("click", function (e) {
     result.innerHTML = "";
     searchResult = [];
     cuisineFilteredList = [];
+    inputSearch = [];
 
     // $ knapparna fungerar och kan filtrera med en eller flera alternativ
     // få till dom i en mindre funktion? kalla på en funktion som ligger utanför denna? 
+
+    // function priceCheck() {
+
+    //     if (priceOneBtn.className === "activeBtn") {
+    //         let priceOneList = restaurantData.filter((restaurant) => {
+    //             return restaurant.price_range.length === 1;
+    //         })
+    //         searchResult = [...searchResult, ...priceOneList];
+
+    //         priceOneBtn.classList.remove("activeBtn");
+
+    //     };
+    //     if (priceTwoBtn.className === "activeBtn") {
+    //         let priceTwoList = restaurantData.filter((restaurant) => {
+    //             return restaurant.price_range.length === 2;
+    //         })
+    //         searchResult = [...searchResult, ...priceTwoList];
+
+    //         priceTwoBtn.classList.remove("activeBtn");
+
+    //     };
+    //     if (priceThreeBtn.className === "activeBtn") {
+    //         let priceThreeList = restaurantData.filter((restaurant) => {
+    //             return restaurant.price_range.length === 3;
+    //         })
+    //         searchResult = [...searchResult, ...priceThreeList];
+
+    //         priceThreeBtn.classList.remove("activeBtn");
+
+    //     };
+    //     if (priceFourBtn.className === "activeBtn") {
+    //         let priceFourList = restaurantData.filter((restaurant) => {
+    //             return restaurant.price_range.length === 4;
+    //         })
+    //         searchResult = [...searchResult, ...priceFourList];
+
+    //         priceFourBtn.classList.remove("activeBtn");
+
+    //     };
+
+    //     return searchResult;
+
+    // }
+
+    // function cuisinecheck() {
+
+    //     priceCheck()
+
+    //     if (searchResult.length >= 1) {
+
+    //         //console.log("activeBtnCuisine[0].value: ", activeBtnCuisine[0].value)
+    //         //console.log(searchResult[0])
+    //         let cuisineFilters = searchResult.filter((restaurant) => {
+    //             return restaurant.cuisines.includes(activeBtnCuisine[0].value);
+    //         });
+
+    //         let cuisineBtnClass = document.getElementsByClassName("activeBtnCuisine");
+    //         [].forEach.call(cuisineBtnClass, function (e) {
+    //             e.classList.remove("activeBtnCuisine");
+    //         });
+    //         //activeBtnCuisine[0].className.remove(activeBtnCuisine)
+
+    //         //console.log("cuisineFilters: ", cuisineFilters)
+    //         cuisineFilteredList = [...cuisineFilteredList, ...cuisineFilters];
+    //     }
+
+    //     return cuisineFilteredList;
+    // }
+
+    cuisinecheck();
+
+    // if (searchResult.length >= 1) {
+
+    //     //console.log("activeBtnCuisine[0].value: ", activeBtnCuisine[0].value)
+    //     //console.log(searchResult[0])
+    //     let cuisineFilters = searchResult.filter((restaurant) => {
+    //         return restaurant.cuisines.includes(activeBtnCuisine[0].value);
+    //     });
+
+    //     let cuisineBtnClass = document.getElementsByClassName("activeBtnCuisine");
+    //     [].forEach.call(cuisineBtnClass, function(e) {
+    //         e.classList.remove("activeBtnCuisine");
+    //     });
+    //     //activeBtnCuisine[0].className.remove(activeBtnCuisine)
+
+    //     //console.log("cuisineFilters: ", cuisineFilters)
+    //     cuisineFilteredList = [...cuisineFilteredList, ...cuisineFilters];
+    // }
+
+    // inputfältets filter efter cuisine..kolla på autocomplete hur det funkar.
+    // vill söka på restaurangnamn och cuisine
+    // kanske ha en funktion utanför man kallar på som kollar om innehållet i inputBox matchar cuisine &/ restName?
+
+    
+
+        if (inputBox.value.length > 1) {
+            let cuisineSearch = restaurantData.filter((restaurant) => {
+                return restaurant.cuisines.find(cuisine => cuisine.includes(inputBox.value));
+            });
+            // let nameSearch = restaurantData.filter((restaurant) => {
+            //     return restaurant.restaurant_name.find(name => name.includes(inputBox.value));
+            // });
+            //console.log("namesearch: ", nameSearch)
+            inputSearch = [...inputSearch, ...cuisineSearch];
+        }
+        
+    
+
+    // if (inputBox.value.length > 1) {
+    //     let cuisineSearch = restaurantData.filter((restaurant) => {
+    //         return restaurant.cuisines.find(cuisine => cuisine.includes(inputBox.value));
+    //     });
+    //     // let nameSearch = restaurantData.filter((restaurant) => {
+    //     //     return restaurant.restaurant_name.find(name => name.includes(inputBox.value));
+    //     // });
+    //     //console.log("namesearch: ", nameSearch)
+    //     inputSearch = [...inputSearch, ...cuisineSearch];
+    // }
+
+    // mappar igenom searchResult och kör funktionen createElement 
+
+    if (cuisineFilteredList.length >= 1) {
+        cuisineFilteredList.map((restaurant) => {
+
+            createElement(restaurant)
+
+        });
+    } else if (inputSearch.length >= 1) {
+        inputSearch.map((restaurant) => {
+
+            createElement(restaurant)
+
+        });
+    } else {
+        console.log("hej")
+        errorMessage();
+        // ONSDAG 9/2 att göra nedan + kolla variabelnamn osvosvosv...
+        // fixa funktion som skriver ut att det inte finns några restauranger och skriv ut kanske vad man sökt efter? 
+        // css: man kan markera hela diven rosa, skriv .activeBtn button för att slippa det i CSS
+    }
+
+
+
+})
+
+function priceCheck() {
+
     if (priceOneBtn.className === "activeBtn") {
         let priceOneList = restaurantData.filter((restaurant) => {
             return restaurant.price_range.length === 1;
@@ -114,17 +263,13 @@ submitBtn.addEventListener("click", function (e) {
 
     };
 
-    // inputfältets filter efter cuisine..kolla på autocomplete hur det funkar.
-    // vill söka på restaurangnamn och cuisine
-    // kanske ha en funktion utanför man kallar på som kollar om innehållet i inputBox matchar cuisine &/ restName?
-    if (inputBox.value.length > 1) {
-        let cuisineSearch = restaurantData.filter((restaurant) => {
-            return restaurant.cuisines.find(cuisine => cuisine.includes(inputBox.value));
-        });
-        searchResult = [...searchResult, ...cuisineSearch];
-    }
+    return searchResult;
 
-    //console.log("searchresult 1; ", searchResult)
+}
+
+function cuisinecheck() {
+
+    priceCheck()
 
     if (searchResult.length >= 1) {
 
@@ -135,7 +280,7 @@ submitBtn.addEventListener("click", function (e) {
         });
 
         let cuisineBtnClass = document.getElementsByClassName("activeBtnCuisine");
-        [].forEach.call(cuisineBtnClass, function(e) {
+        [].forEach.call(cuisineBtnClass, function (e) {
             e.classList.remove("activeBtnCuisine");
         });
         //activeBtnCuisine[0].className.remove(activeBtnCuisine)
@@ -144,28 +289,8 @@ submitBtn.addEventListener("click", function (e) {
         cuisineFilteredList = [...cuisineFilteredList, ...cuisineFilters];
     }
 
-    //console.log("cuisineFilteredList: ", cuisineFilteredList.length)
-    //console.log("cuisineFilteredList: ", cuisineFilteredList)
-
-    // mappar igenom searchResult och kör funktionen createElement 
-    
-
-    if (cuisineFilteredList.length >= 1) {
-        cuisineFilteredList.map((restaurant) => {
-
-            createElement(restaurant)
-    
-        });
-    } else {
-        console.log("hej")
-        // ONSDAG 9/2 att göra nedan + kolla variabelnamn osvosvosv...
-        // fixa funktion som skriver ut att det inte finns några restauranger och skriv ut kanske vad man sökt efter? 
-        // css: man kan markera hela diven rosa, skriv .activeBtn button för att slippa det i CSS
-    }
-
-
-
-})
+    return cuisineFilteredList;
+}
 
 // eventlyssnare för varje $ knapp som togglar activeBtn classen
 priceOneBtn.addEventListener("click", function () {
@@ -208,54 +333,64 @@ cuisineChoise.addEventListener("click", function (e) {
 
 function createElement(element) {
 
-    //data.forEach(element => {
 
-        let div = document.createElement("div");
+    let div = document.createElement("div");
 
-        // restaurant name
-        let restaurantName = document.createElement("h4");
-        restaurantName.innerText = element.restaurant_name;
-        div.appendChild(restaurantName);
+    // restaurant name
+    let restaurantName = document.createElement("h4");
+    restaurantName.innerText = element.restaurant_name;
+    div.appendChild(restaurantName);
 
-        // cuisine
-        let restaurantCuisine = document.createElement("p");
-        restaurantCuisine.innerText = element.cuisines;
-        div.appendChild(restaurantCuisine);
+    // cuisine
+    let restaurantCuisine = document.createElement("p");
+    restaurantCuisine.innerText = element.cuisines;
+    div.appendChild(restaurantCuisine);
 
-        // price range
-        let priceRange = document.createElement("p");
-        priceRange.innerText = element.price_range;
-        div.appendChild(priceRange);
+    // price range
+    let priceRange = document.createElement("p");
+    priceRange.innerText = element.price_range;
+    div.appendChild(priceRange);
 
-        // geo.. long & lat
-        let geo = document.createElement("p");
-        geo.innerText = calcCrow(element.geo.lat, element.geo.lon, latitudeInput.innerText, longitudeInput.innerText);
-        geo.classList = "distanceClass";
-        div.appendChild(geo);
+    // geo.. long & lat
+    let geo = document.createElement("p");
+    geo.innerText = calcCrow(element.geo.lat, element.geo.lon, latitudeInput.innerText, longitudeInput.innerText);
+    geo.classList = "distanceClass";
+    div.appendChild(geo);
 
-        //heart
-        let heart = document.createElement("p");
-        //heart.innerHTML = '<i class="far fa-heart"></i>';
-        heart.innerHTML = '<i class="far fa-heart"></i>';
-        heart.classList = "heartBtn";
-        heart.id = "heart";
-        div.appendChild(heart);
+    //heart
+    let heart = document.createElement("p");
+    //heart.innerHTML = '<i class="far fa-heart"></i>';
+    heart.innerHTML = '<i class="far fa-heart"></i>';
+    heart.classList = "heartBtn";
+    heart.id = "heart";
+    div.appendChild(heart);
 
-        // website
-        if (element.restaurant_website.length > 1) {
-            let restaurantWebsite = document.createElement("a");
-            restaurantWebsite.href = element.restaurant_website;
-            restaurantWebsite.innerText = "Restaurants website";
-            restaurantWebsite.setAttribute("target", "_blank");
-            div.appendChild(restaurantWebsite);
-        }
+    // website
+    if (element.restaurant_website.length > 1) {
+        let restaurantWebsite = document.createElement("a");
+        restaurantWebsite.href = element.restaurant_website;
+        restaurantWebsite.innerText = "Restaurants website";
+        restaurantWebsite.setAttribute("target", "_blank");
+        div.appendChild(restaurantWebsite);
+    }
 
-        // adding to result div
-        result.appendChild(div);
-    //});
-
+    // adding to result div
+    result.appendChild(div);
 
 }
+
+function errorMessage() {
+
+    let div = document.createElement("div");
+    div.id = "errorDiv"
+    let p = document.createElement("p");
+    p.innerText = "Vänligen försök igen. Det fanns inga restauranger med dina önskemål";
+    div.appendChild(p);
+
+    result.appendChild(div);
+}
+
+
 
 // ---------- Distance func ----------
 //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
